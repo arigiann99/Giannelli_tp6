278bd021abbfc2327704c96ce74c4b3be020daad
Ariel Giannelli <arielgiannelli@gmail.com>
2020-09-11 20:05:37 -0300

    Trabajo Practico - Lab I

---
+++ src/tp/pkg06/Matriz.java
@@ -0,0 +1,28 @@
+
+package tp.pkg06;
+
+
+public class Matriz {
+    
+    private int matriz [][] = { {3,6,9,12},{15,18,21},{24,27,30,33,36},{39,42} };
+       
+    public void armar(){
+        System.out.println("La Matriz es: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {               
+                    System.out.print(matriz[i][j]+" ");                   
+            }
+                System.out.println("");             
+        }
+        System.out.println("");             
+        System.out.println("Numeros Pares de la Matriz: ");
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz[i].length; j++) {
+                if(matriz[i][j]%2==0){
+                    System.out.print(matriz[i][j]+" ");   
+                }
+            }
+                System.out.println("");             
+        }
+    }
+}
---
+++ src/tp/pkg06/TestMatriz.java
@@ -0,0 +1,15 @@
+
+package tp.pkg06;
+
+public class TestMatriz {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Matriz m = new Matriz();
+        
+        m.armar();
+    }
+    
+}
