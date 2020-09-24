c8623bee312ecabb68c2b90689a7eb5e727b8e44
Ariel Giannelli <arielgiannelli@gmail.com>
2020-09-24 11:03:41 -0300

    Trabajo Practico 6 subida a git

--- src/tp/pkg06/Matriz.java
+++ src/tp/pkg06/Matriz.java
@@ -4,13 +4,24 @@ package tp.pkg06;
 
 public class Matriz {
     
-    private int matriz [][] = { {3,6,9,12},{15,18,21},{24,27,30,33,36},{39,42} };
-       
+    //private int matriz [][] = { {3,6,9,12},{15,18,21},{24,27,30,33,36},{39,42} }; // armado de matriz manual
+   
     public void armar(){
+        
+        int matriz [][]=new int[4][]; // armado de matriz con serie de numeros(automatico)
+        matriz[0]= new int[4];
+        matriz[1]= new int[3];
+        matriz[2]= new int[5];
+        matriz[3]= new int[2];
+        
+        int contador = 3;
+        
         System.out.println("La Matriz es: ");
         for (int i = 0; i < matriz.length; i++) {
-            for (int j = 0; j < matriz[i].length; j++) {               
-                    System.out.print(matriz[i][j]+"");                   
+            for (int j = 0; j < matriz[i].length; j++) {
+                    matriz[i][j]=contador;
+                    contador += 3; 
+                    System.out.print(matriz[i][j]+" ");                   
             }
                 System.out.println("");             
         }
